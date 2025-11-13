2f9c64997a5bc54b3c91bb5202277d4e35b2a531
pragatisaste0021 <103356760+pragatisaste0021@users.noreply.github.com>
2025-11-13 20:16:04 +0530

    Update message.controller.js

--- Backend/controller/message.controller.js
+++ Backend/controller/message.controller.js
@@ -9,7 +9,7 @@ export const sendMessage=async(req, res) => {
         const {message} = req.body;
         const {id: receiverId} = req.params;
         const senderId = req.user._id;
-
+        
         let conversation = await Conversation.findOne({
             members: {$all:[senderId, receiverId]}
         })
